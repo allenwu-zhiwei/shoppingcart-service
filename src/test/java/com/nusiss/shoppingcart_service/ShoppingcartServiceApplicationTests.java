6821e8a32b597b190a3e0191c43a81847a391dca
ljr <122674853+jerry975@users.noreply.github.com>
2024-10-28 10:42:08 +0800

    Update ShoppingcartServiceApplicationTests.java

--- src/test/java/com/nusiss/shoppingcart_service/ShoppingcartServiceApplicationTests.java
+++ src/test/java/com/nusiss/shoppingcart_service/ShoppingcartServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ShoppingcartServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
