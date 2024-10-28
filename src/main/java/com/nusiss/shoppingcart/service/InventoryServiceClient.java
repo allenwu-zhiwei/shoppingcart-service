2619008b2a045393d23df8244c4ff5fba61cce7d
ljr <122674853+jerry975@users.noreply.github.com>
2024-10-28 17:47:20 +0800

    Update InventoryServiceClient.java

--- src/main/java/com/nusiss/shoppingcart/service/InventoryServiceClient.java
+++ src/main/java/com/nusiss/shoppingcart/service/InventoryServiceClient.java
@@ -3,7 +3,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory")
+@FeignClient(name = "inventory-service")
 public interface InventoryServiceClient {
     @GetMapping("/check/{productId}/{num}")
     boolean check(@PathVariable("productId") Long productId, @PathVariable("num") int num);
