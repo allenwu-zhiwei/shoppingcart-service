9fe29e6947484701e0f7e3555eba838d19e68217
ljr <122674853+jerry975@users.noreply.github.com>
2024-10-28 17:47:35 +0800

    Update ProductServiceClient.java

--- src/main/java/com/nusiss/shoppingcart/service/ProductServiceClient.java
+++ src/main/java/com/nusiss/shoppingcart/service/ProductServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "product")
+@FeignClient(name = "product-service")
 public interface ProductServiceClient {
     @GetMapping("/product")
     ResponseEntity<ApiResponse<ProductDTO>> productInfo(@RequestParam("productId") Long productId);
